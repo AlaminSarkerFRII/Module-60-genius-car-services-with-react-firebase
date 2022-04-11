363da89404b29e827100e64e25ebc11c4853ebf2
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-04-11 19:14:51 +0600

    added experst

---
+++ src/Pages/Home/Experts/Experts.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import expert1 from "../../../imgs/experts/expert-1.jpg";
+import expert2 from "../../../imgs/experts/expert-2.jpg";
+import expert3 from "../../../imgs/experts/expert-3.jpg";
+import expert4 from "../../../imgs/experts/expert-4.jpg";
+import expert5 from "../../../imgs/experts/expert-5.jpg";
+import expert6 from "../../../imgs/experts/expert-6.png";
+
+const experts = [
+  { id: 1, name: "wiil Smith", img: expert1 },
+  { id: 2, name: "Kill Smith", img: expert2 },
+  { id: 3, name: "Mill Smith", img: expert3 },
+  { id: 4, name: "Nill Smith", img: expert4 },
+  { id: 5, name: "Bill Smith", img: expert5 },
+  { id: 6, name: "Siil Smith", img: expert6 },
+];
+
+const Experts = () => {
+  return (
+    <div>
+      <h2>Our Experts</h2>
+    </div>
+  );
+};
+
+export default Experts;
