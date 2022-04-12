b58a43d4429cc5a47f667068ecf8ab3f80431bbf
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-04-12 23:18:27 +0600

    register commponent added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,65 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  // useref
+  const emailRef = useRef(""); // string value empty thakbe
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+
+  // handle submit
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email, password);
+  };
+
+  const navigateToRegister = (event) => {
+    navigate("/register");
+  };
+
   return (
-    <div>
-      <h2> Please Login</h2>
+    <div className="container w-50 mx-auto py-2">
+      <h2 className="text-center text-primary"> Please Login</h2>
+      <Form onSubmit={handleOnSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <p>
+        New to Genius Car ?
+        <span onClick={navigateToRegister} className="text-danger px-3 fw-bold">
+          Please Register
+        </span>
+      </p>
     </div>
   );
 };
